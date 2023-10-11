6c3d240f8119630f76aa6ba27fc24859c48d517e
tomas marelli <tomasexequielmarelli68@gmail.com>
2023-10-11 17:20:54 -0300

    nuevos

--- javascript/script.js
+++ javascript/script.js
@@ -1,129 +1,104 @@
+// Inicializamos un array para almacenar los productos
+const productos = [];
 
-// Variables para almacenar los productos
-let nombreProducto1, precioProducto1;
-let nombreProducto2, precioProducto2;
-let nombreProducto3, precioProducto3;
-
-// Función para agregar un producto
-function agregarProducto() {
-  const nombre = prompt('Ingrese el nombre del producto:');
-  const precio = parseFloat(prompt('Ingrese el precio del producto:'));
-  
-  if (!nombre || isNaN(precio)) {
-    alert('Entrada inválida. El producto no se ha agregado.');
-  } else if (!nombreProducto1) {
-    nombreProducto1 = nombre;
-    precioProducto1 = precio;
-    alert(`El producto "${nombre}" con precio $${precio.toFixed(2)} ha sido agregado.`);
-  } else if (!nombreProducto2) {
-    nombreProducto2 = nombre;
-    precioProducto2 = precio;
-    alert(`El producto "${nombre}" con precio $${precio.toFixed(2)} ha sido agregado.`);
-  } else if (!nombreProducto3) {
-    nombreProducto3 = nombre;
-    precioProducto3 = precio;
-    alert(`El producto "${nombre}" con precio $${precio.toFixed(2)} ha sido agregado.`);
-  } else {
-    alert('Ya ha alcanzado el límite de productos que puede almacenar.');
-  }
+// Función para agregar un producto al array de productos
+function agregarProducto(nombre, precio, cantidad) {
+  const producto = {
+    nombre: nombre,
+    precio: precio,
+    cantidad: cantidad,
+  };
+  productos.push(producto);
+  alert(`Producto "${nombre}" agregado con éxito.`);
 }
 
-// Función para modificar un producto
-function modificarProducto() {
-  const nombre = prompt('Ingrese el nombre del producto que desea modificar:');
-  
-  if (nombre === nombreProducto1) {
-    const nuevoPrecio = parseFloat(prompt(`Ingrese el nuevo precio para "${nombre}":`));
-  
-    if (isNaN(nuevoPrecio)) {
-      alert('Entrada inválida. El producto no se ha modificado.');
-    } else {
-      precioProducto1 = nuevoPrecio;
-      alert(`El precio del producto "${nombre}" ha sido actualizado a $${nuevoPrecio.toFixed(2)}.`);
-    }
-  } else if (nombre === nombreProducto2) {
-    const nuevoPrecio = parseFloat(prompt(`Ingrese el nuevo precio para "${nombre}":`));
-  
-    if (isNaN(nuevoPrecio)) {
-      alert('Entrada inválida. El producto no se ha modificado.');
-    } else {
-      precioProducto2 = nuevoPrecio;
-      alert(`El precio del producto "${nombre}" ha sido actualizado a $${nuevoPrecio.toFixed(2)}.`);
-    }
-  } else if (nombre === nombreProducto3) {
-    const nuevoPrecio = parseFloat(prompt(`Ingrese el nuevo precio para "${nombre}":`));
-  
-    if (isNaN(nuevoPrecio)) {
-      alert('Entrada inválida. El producto no se ha modificado.');
-    } else {
-      precioProducto3 = nuevoPrecio;
-      alert(`El precio del producto "${nombre}" ha sido actualizado a $${nuevoPrecio.toFixed(2)}.`);
-    }
-  } else {
-    alert('El producto no existe en la lista.');
-  }
+// Función para mostrar la lista de productos
+function mostrarProductos() {
+  let listaProductos = "Lista de Productos:\n";
+  productos.forEach((producto, index) => {
+    listaProductos += `${index + 1}. Nombre: ${producto.nombre}, Precio: $${producto.precio}, Cantidad: ${producto.cantidad}\n`;
+  });
+  alert(listaProductos);
 }
 
-// Función para eliminar un producto
-function eliminarProducto() {
-  const nombre = prompt('Ingrese el nombre del producto que desea eliminar:');
-  
-  if (nombre === nombreProducto1) {
-    nombreProducto1 = undefined;
-    precioProducto1 = undefined;
-    alert(`El producto "${nombre}" ha sido eliminado.`);
-  } else if (nombre === nombreProducto2) {
-    nombreProducto2 = undefined;
-    precioProducto2 = undefined;
-    alert(`El producto "${nombre}" ha sido eliminado.`);
-  } else if (nombre === nombreProducto3) {
-    nombreProducto3 = undefined;
-    precioProducto3 = undefined;
-    alert(`El producto "${nombre}" ha sido eliminado.`);
-  } else {
-    alert('El producto no existe en la lista.');
-  }
-}
-
-// Menú principal
-let bandera = true;
-while (bandera) {
-  const opcion = prompt(
-    'Seleccione una opción:\n' +
-    '1. Agregar producto\n' +
-    '2. Modificar producto\n' +
-    '3. Eliminar producto\n' +
-    '4. Salir'
+// Función para buscar un producto por nombre
+function buscarProductoPorNombre(nombre) {
+  const resultado = productos.filter((producto) =>
+    producto.nombre.toLowerCase() === nombre.toLowerCase()
   );
-  
-  switch (opcion) {
-    case '1':
-      agregarProducto();
-      break;
-    case '2':
-      modificarProducto();
-      break;
-    case '3':
-      eliminarProducto();
-      break;
-    case '4':
-      alert('Saliendo del programa.');
-      // Salir del bucle y finalizar el programa
-      bandera=false;
-      break ;
-    default:
-      alert('Opción no válida. Intente de nuevo.');
-  }
 
+  if (resultado.length > 0) {
+    let mensaje = `Resultado de la búsqueda para "${nombre}":\n`;
+    resultado.forEach((producto, index) => {
+      mensaje += `${index + 1}. Nombre: ${producto.nombre}, Precio: $${producto.precio}, Cantidad: ${producto.cantidad}\n`;
+    });
+    alert(mensaje);
+  } else {
+    alert(`No se encontraron productos con el nombre "${nombre}".`);
+  }
 }
 
+// Función para filtrar productos por precio
+function filtrarProductosPorPrecio(precioMin, precioMax) {
+  const resultado = productos.filter((producto) =>
+    producto.precio >= precioMin && producto.precio <= precioMax
+  );
 
+  if (resultado.length > 0) {
+    let mensaje = `Productos con precio entre $${precioMin} y $${precioMax}:\n`;
+    resultado.forEach((producto, index) => {
+      mensaje += `${index + 1}. Nombre: ${producto.nombre}, Precio: $${producto.precio}, Cantidad: ${producto.cantidad}\n`;
+    });
+    alert(mensaje);
+  } else {
+    alert("No se encontraron productos en ese rango de precios.");
+  }
+}
 
+// Función para mostrar el menú
+function mostrarMenu() {
+  return prompt(
+    "Seleccione una opción:\n1. Agregar Producto\n2. Mostrar Productos\n3. Buscar Producto por Nombre\n4. Filtrar Productos por Precio\n5. Salir"
+  );
+}
 
+// Función para iniciar el simulador
+function iniciarSimulador() {
+  let continuar = true;
 
+  while (continuar) {
+    const opcion = mostrarMenu();
 
+    switch (opcion) {
+      case "1":
+        const nombre = prompt("Ingrese el nombre del producto:");
+        const precio = parseFloat(prompt("Ingrese el precio del producto:"));
+        const cantidad = parseInt(prompt("Ingrese la cantidad del producto:"));
+        agregarProducto(nombre, precio, cantidad);
+        break;
+      case "2":
+        mostrarProductos();
+        break;
+      case "3":
+        const nombreBusqueda = prompt("Ingrese el nombre del producto a buscar:");
+        buscarProductoPorNombre(nombreBusqueda);
+        break;
+      case "4":
+        const precioMin = parseFloat(prompt("Ingrese el precio mínimo:"));
+        const precioMax = parseFloat(prompt("Ingrese el precio máximo:"));
+        filtrarProductosPorPrecio(precioMin, precioMax);
+        break;
+      case "5":
+        continuar = false;
+        alert("Saliendo del simulador.");
+        break;
+      default:
+        alert("Opción no válida. Por favor, seleccione una opción válida.");
+        break;
+    }
+  }
+}
 
-
-
-
+// Iniciar el simulador
+iniciarSimulador();
 
